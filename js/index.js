e85663a82d23960c85786e283bc9b86570b3d966
Alexandra Fren <alexandra.fren@gmail.com>
2019-01-11 15:46:54 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 // your code here
 function getRepositories() {
+  let username = document.getElementById('username').value;
   const req = new XMLHttpRequest();
   req.addEventListener('load', displayRepositories);
   req.open('GET', 'https://api.github.com/users/' + username + '/repos');
