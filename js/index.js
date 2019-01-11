a6de3594951620344bb1d86e031f50964d79c405
Alexandra Fren <alexandra.fren@gmail.com>
2019-01-11 16:07:41 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -10,15 +10,14 @@ function getRepositories() {
 function displayRepositories() {
   var repos = JSON.parse(this.responseText);
   console.log(repos);
-  const repoList = `<ul>${repos
-    .map(
-      r =>
-        '<li>' +
-        r.name +
-        ' - <a href="#" data-repo="' +
-        r.name +
-        '" onclick="getCommits(this)">Get Commits</a></li>'
-    )
+  const repoList = `<ul>${repos.map(
+    r =>
+      '<li>' +
+        '<a href="https://github.com/' + r.owner.login + '/' + r.name + '">' + r.name + '</a>' +
+        ' - <a href="#" data-repository="' + r.name + '" data-username="' + r.owner.login + '" onclick="getCommits(this)">Get Commits</a>' +
+        ' - <a href="#" data-repository="' + r.name + '" data-username="' + r.owner.login + '" onclick="getBranches(this)">Get Branches</a>' +
+      '</li>'
+  )
     .join('')}</ul>`;
   document.getElementById('repositories').innerHTML = repoList;
 }
